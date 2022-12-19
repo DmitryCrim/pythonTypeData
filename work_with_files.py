ca9f2f6578eb54585b194b8832c64003cf4fc06a
Dmitry Trakhimovich <dmitrytrakhimovich@google.com>
2022-12-19 20:45:22 +0300

    read data

--- work_with_files.py
+++ work_with_files.py
@@ -1,7 +1,12 @@
-var = input('Write something')
-fw = open('doc/file.txt', 'a')
-fw.write(var)
-fw.close()
+# var = input('Write something')
+# fw = open('doc/file.txt', 'a')
+# fw.write(var)
+# fw.close()
 
-a - запись новых данных в конец старых
-w - запись новых данных (удаляя старые данные)
+# a - запись новых данных в конец старых
+# w - запись новых данных (удаляя старые данные)
+
+fr = open ('doc/file.txt' , 'r')
+text = fr.read()
+fr.close()
+print (text)
